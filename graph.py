9a9df3d49e162e16d61b11a7dcfa99e86ac0d162
Pranav Maddineedi <77818951+Pramad712@users.noreply.github.com>
2024-04-18 19:17:34 -0700

    Update graph.py

--- graph.py
+++ graph.py
@@ -1,12 +1,31 @@
-from typing import Any
 from dataclasses import dataclass, field
 import numpy
 import pygame
 import constants
 
+
 @dataclass
 class Node:
-    x: int
-    y: int
-    color: tuple[int, int, int]
-    representation: str #
+   x: int
+   y: int
+   color: tuple[int, int, int]
+   type_: str # either: empty, a number (classroom, use .is), obstacle
+
+
+   def __hash__(self):
+       return hash((self.x, self.y))
+
+
+   def __repr__(self):
+       return self.type_ if self.type_.isnumeric() else "X" if self.type_ == "obstacle" else " " if self.type_ == "stairs" else " "
+
+
+@dataclass
+class NodePathData:                                                                                                                                                                                                                                                                                                                                                                                                                                  
+   node: Node
+   from_: 'NodePathData'
+   distance: float
+   heuristic: float
+
+
+def create_graph() -> list[list[Node]]:
