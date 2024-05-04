b3da92a91be9a03251f9061c7636c169c916af5d
Pranav Maddineedi <77818951+Pramad712@users.noreply.github.com>
2024-05-04 16:18:57 -0700

    Update constants.py

--- constants.py
+++ constants.py
@@ -1,26 +1,21 @@
 from math import sqrt
 import pygame
 
-LENGTH, HEIGHT = 800, 600 # Fills the screen on an Amazon Fire 7
+LENGTH, HEIGHT = 800, 600
 
 DIAGONAL_DISTANCE = sqrt(2)
-DIAGONAL_DISTANCE_REPR = "√2"
 STAIRS_DISTANCE = 39.6
 SCALE = 13/12
 
-WHITE = (255, 255, 255) # 100% Luminosity
-BLACK = (0, 0, 0) # 0%
-GREY = (128, 128, 128) # 50%
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+GREY = (128, 128, 128)
 
-BLUE = (0, 0, 240) # Most classrooms
-BROWN = (160, 82, 45) # Some classrooms
-LIGHT_BROWN = (255, 248, 220) # Some classrooms
-
+BLUE = (0, 0, 240)
 GREEN = (0, 230, 0)
 RED = (230, 0, 0)
-YELLOW = (255, 165, 0)
+ORANGE = (255, 165, 0)
 
-# Fonts - all but the last are for the room labels, "Start" and "End"
 pygame.font.init()
 MICRO_FONT = pygame.font.SysFont("comicsans", 7)
 TINY_FONT = pygame.font.SysFont("comicsans", 8)
@@ -30,10 +25,3 @@ BIG_FONT = pygame.font.SysFont("comicsans", 25)
 HUGE_FONT = pygame.font.SysFont("comicsans", 40)
 CREDITS_FONT = pygame.font.SysFont("comicsans", 15)
 TYPING_SIZE_FONT = pygame.font.SysFont("comicsans", 30)
-
-def decimal_range(start, stop, step=1):
-    value = start
-
-    while value < stop:
-        yield value
-        value += step
