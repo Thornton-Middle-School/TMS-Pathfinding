a2789e056f9c80fe2a154ccd2a44a5e588186cbe
Pranav Maddineedi <77818951+Pramad712@users.noreply.github.com>
2024-05-04 20:03:33 -0700

    Update graph.py

--- graph.py
+++ graph.py
@@ -30,7 +30,7 @@ class Node:
 
     def __eq__(self, other):
         return self.__dict__ == other.__dict__
-        
+
 def upstairs(node: Node | str):
     if isinstance(node, Node):
         return node.corner_y < 224
